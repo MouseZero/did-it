4261c32df3cde4d2a722467704de4ee47a070acd
thebearingedge <contact@timdav.is>
2019-02-15 21:38:36 -0800

    Add change and submit handlers to habit creation form. #1

--- src/CreateHabit.jsx
+++ src/CreateHabit.jsx
@@ -1,12 +1,46 @@
-import React from "react"
+import uuid from "uuid/v4"
+import React, { Component } from "react"
+import { HABIT_KEY } from './constants'
 
-function CreateHabit(props) {
-  return (
-    <>
-      <input type="text" />
-      <button>Submit</button>
-    </>
-  )
+export default class CreateHabit extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fields: {
+        title: ""
+      }
+    }
+  }
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        [name]: value
+      }
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    const habits = JSON.parse(localStorage.getItem(HABIT_KEY)) || {}
+    const habit = {
+      habitId: uuid(),
+      ...this.state.fields
+    }
+    localStorage.setItem(
+      HABIT_KEY,
+      JSON.stringify({
+        ...habits,
+        [habit.habitId]: habit
+      })
+    )
+  }
+  render() {
+    const { handleChange, handleSubmit } = this
+    return (
+      <form onSubmit={handleSubmit}>
+        <input required name="title" type="text" onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
 }
-
-export default CreateHabit

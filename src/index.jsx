a7c77050b4f367b63f5b1c34861737fd4f0269e2
MouseZero <rm@mousezero.com>
2019-02-15 21:58:36 -0800

    #1 Style page

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-import CreateHabit from "./CreateHabit.jsx"
+import CreateHabit from "./CreateHabit.jsx";
 
-ReactDOM.render(<CreateHabit />, document.getElementById("root"))
+ReactDOM.render(
+  <div style={{ marginTop: "3rem" }} className="container">
+    <div className="row">
+      <div className="column">
+        <CreateHabit />
+      </div>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
